077af1b36377356b945d6b24a216fc463acfbc23
leo liu <scau_lzc@163.com>
2019-11-19 19:39:54 +0800

    add resource load class

---
+++ src/main/java/org/myspring/factory/config/resource/Resource.java
@@ -0,0 +1,12 @@
+package org.myspring.factory.config.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * 读取文件的类，本来应该是InputStream接口的，Resource接口是做文件操作的，为了简便直接在Resource接口里操作字节流
+ */
+public interface Resource {
+    InputStream getInputStream() throws IOException;
+}
---
+++ src/main/java/org/myspring/factory/config/resource/ResourceLoader.java
@@ -0,0 +1,20 @@
+package org.myspring.factory.config.resource;
+
+import java.net.URL;
+
+public class ResourceLoader {
+    /**
+     * 用类加载器读取项目下的文件
+     * @param location
+     * @return
+     */
+    public Resource getResource(String location){
+        Class<? extends ResourceLoader> aClass = this.getClass();
+        System.out.println("Class: " + aClass);
+        ClassLoader classLoader = aClass.getClassLoader();
+        System.out.println("ClassLoader: " + classLoader);
+        URL resource = classLoader.getResource(location);
+        System.out.println("URL resource: " + resource);
+        return new UrlResource(resource);
+    }
+}
