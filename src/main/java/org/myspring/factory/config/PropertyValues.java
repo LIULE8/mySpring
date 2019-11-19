e9afc5816a1a830773c75876dfac9a93fbdeeb2f
Leo Liu <LEO.ZHEN.CONG.LIU@OOCL.COM>
2019-11-19 17:27:23 +0800

    update addPropertyValues method

--- src/main/java/org/myspring/factory/config/PropertyValues.java
+++ src/main/java/org/myspring/factory/config/PropertyValues.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
  * Bean的属性
+ *
  * @author Leo Liu
  * @create 19/11/2019
  */
@@ -13,14 +13,18 @@ public class PropertyValues {
 
   private final List<PropertyValue> propertyValueList = new ArrayList<>(256);
 
-  public void addPropertyValues(String propertyName, Object propertyValue){
-      PropertyValue pv = new PropertyValue(propertyName, propertyValue);
-      for (int i = 0; i < propertyValueList.size(); i++) {
-          PropertyValue currentPv = propertyValueList.get(i);
-          if (currentPv.getName().equals(pv.getName())){
-
-          }
+  public void addPropertyValues(String propertyName, Object propertyValue) {
+    PropertyValue pv = new PropertyValue(propertyName, propertyValue);
+    for (int i = 0; i < propertyValueList.size(); i++) {
+      PropertyValue currentPv = propertyValueList.get(i);
+      if (currentPv.getName().equals(pv.getName())) {
+        // 省略merge操作，直接替换
+        propertyValueList.set(i, pv);
       }
+    }
   }
 
+  public List<PropertyValue> getPropertyValueList() {
+    return propertyValueList;
+  }
 }
