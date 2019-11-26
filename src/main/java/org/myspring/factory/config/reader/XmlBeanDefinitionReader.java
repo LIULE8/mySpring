65c0575df82fd2b4977349322f7236122c3ff3d5
Leo Liu <scau_lzc@163.com>
2019-11-26 21:02:17 +0800

    use dom4J read xml content and convert to beanDefinition and save in registry map

--- src/main/java/org/myspring/factory/config/reader/XmlBeanDefinitionReader.java
+++ src/main/java/org/myspring/factory/config/reader/XmlBeanDefinitionReader.java
@@ -1,19 +1,19 @@
 package org.myspring.factory.config.reader;
 
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.myspring.factory.config.bean.BeanDefinition;
 import org.myspring.factory.config.bean.BeanReference;
 import org.myspring.factory.config.bean.PropertyValue;
 import org.myspring.factory.config.io.ResourceLoader;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.util.List;
 
 /**
+ * 使用Dom4J操作xml的内容，并封装成beanDefinition，存进 registry<String, Definition>里
+ *
  * @author Leo Liu
  * @create 19/11/2019
  */
@@ -30,51 +30,49 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
     }
 
     private void doLoadBeanDefinitions(InputStream inputStream) throws Exception {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = factory.newDocumentBuilder();
-        Document doc = docBuilder.parse(inputStream);
-        // 解析bean
+        SAXReader saxReader = new SAXReader();
+        Document doc = saxReader.read(inputStream);
+        // 解析 bean
         registerBeanDefinitions(doc);
         inputStream.close();
     }
 
     private void registerBeanDefinitions(Document doc) {
-        Element root = doc.getDocumentElement();
+        Element root = doc.getRootElement();
         parseBeanDefinitions(root);
     }
 
+    @SuppressWarnings("all")
     private void parseBeanDefinitions(Element root) {
-        NodeList nl = root.getChildNodes();
-        for (int i = 0; i < nl.getLength(); i++) {
-            Node node = nl.item(i);
-            if (node instanceof Element) {
-                Element ele = (Element) node;
-                processBeanDefinition(ele);
+        List elements = root.elements();
+        for (Object element : elements) {
+            if (element instanceof Element) {
+                processBeanDefinition((Element) element);
             }
         }
     }
 
-    private void processBeanDefinition(Element ele) {
-        String name = ele.getAttribute("id");
-        String className = ele.getAttribute("class");
+    private void processBeanDefinition(Element element) {
+        String name = element.attributeValue("id");
+        String className = element.attributeValue("class");
         BeanDefinition beanDefinition = new BeanDefinition();
-        processProperty(ele, beanDefinition);
+        processProperty(element, beanDefinition);
         beanDefinition.setBeanClassName(className);
         getRegistry().put(name, beanDefinition);
     }
 
-    private void processProperty(Element ele, BeanDefinition beanDefinition) {
-        NodeList propertyNodes = ele.getElementsByTagName("property");
-        for (int i = 0; i < propertyNodes.getLength(); i++) {
-            Node node = propertyNodes.item(i);
+    @SuppressWarnings("all")
+    private void processProperty(Element element, BeanDefinition beanDefinition) {
+        List propertyNodes = element.elements("property");
+        for (Object node : propertyNodes) {
             if (node instanceof Element) {
-                Element properties = (Element) node;
-                String name = properties.getAttribute("name");
-                String value = properties.getAttribute("value");
+                Element property = ((Element) node);
+                String name = property.attributeValue("name");
+                String value = property.attributeValue("value");
                 if (value != null && value.length() > 0) {
                     beanDefinition.getPropertyValues().addPropertyValue(new PropertyValue(name, value));
                 } else {
-                    String ref = properties.getAttribute("ref");
+                    String ref = property.attributeValue("ref");
                     if (ref == null || ref.length() == 0) {
                         throw new IllegalArgumentException("Configuration problem: <property> element for property '"
                                 + name + "' must specify a ref or value");
